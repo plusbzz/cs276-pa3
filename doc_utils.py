18f3fa6bfeaf075104b24a9644d146fe6d01f9a7
Raj Bandyopadhyay <plusbzz@gmail.com>
2013-05-04 10:22:19 -0400

    Add utility functions and classes

--- doc_utils.py
+++ doc_utils.py
@@ -37,19 +37,29 @@ class Anchor(object):
         self.count = anchor_count
  
 class Document(object):
-    def compute_tf_vector(self,words):
+    @staticmethod
+    def compute_tf_vector(words):
         tf = {}
         for w in words:
             if w not in tf: tf[w] = 0
             tf[w] += 1
         return tf
-
-    def compute_tf_idf_vector(self,words,corpus):
-        tf = self.compute_tf_vector(words)
+    
+    @staticmethod
+    def compute_tf_idf_vector(words,corpus):
+        tf = Document.compute_tf_vector(words)
         for w in tf:
             tf[w] *= corpus.get_IDF(w)
-        
-class Page(Document):
+        return tf
+    
+    @staticmethod
+    def cosine_sim(tf1,tf2):
+        tot = 0.0
+        for term in [k for k in tf1 if k in tf2]:
+            tot += (tf1[term]*tf2[term])
+        return tot
+    
+class Page(object):
     def __init__(self,page,page_fields):
         self.url = page
         self.body_length = page_fields.get('body_length',0)
@@ -58,17 +68,27 @@ class Page(Document):
         self.body_hits = page_fields.get('body_hits',0)
         self.anchors = [Anchor(text,count) for text,count in page_fields.get('anchors',{}).iteritems()]
  
-        self.tf_vectors = {}
+        self.field_tf_vectors = self.compute_field_tf_vectors()
+    
+    def body_hits_to_tf_vector(self):
+        tf = {}
+        l = float(self.body_length)
         
-    def compute_tf_vectors(self):
-        pass
+        for bh in body_hits:
+            tf[bh] = len(body_hits[bh])/l
+        
+        return tf
+           
+    def compute_field_tf_vectors(self):
+        tfs = {}
+        tfs['body'] = self.body_hits_to_tf_vector()
          
         
-class Query(Document):
+class Query(object):
     def __init__(self,query,query_pages,corpus):  # query_pages : query -> urls
         self.query = query
         self.terms = self.query.strip().split()
         self.pages = dict([(p,Page(p,v)) for p,v in query_pages.iteritems()]) # URLs
-        self.tf_vector = self.compute_tf_idf_vector(self.terms,corpus)
+        self.tf_vector = Document.compute_tf_idf_vector(self.terms,corpus)
 
         
